f1bac01e934fcdf43b620bd00e8c5c9e4d28ac37
Chen Yu Lin <qwe111845@gmail.com>
2021-03-08 00:03:11 +0800

    feat:Spring MVC Custom Validation
    1.Create custom validation rule
     a.add @interface CourseCode
     b.add CourseCodeConstraintValidator
    2.Add validation rule to Customer class

--- spring-mvc-demo/WebContent/WEB-INF/view/customer/hello-customer.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/customer/hello-customer.jsp
@@ -14,6 +14,7 @@
 		<h2>customer name:${customer.firstName}, ${customer.lastName} </h2>
 		<h2>customer passes:${customer.freePasses} </h2>
 		<h2>Postal Code:${customer.postalCode} </h2>
+		<h2>Course Code:${customer.courseCode} </h2>
 	
 	</body>
 </html>
--- spring-mvc-demo/src/com/lin/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/lin/springdemo/mvc/Customer.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.lin.springdemo.mvc.validation.CourseCode;
+
 public class Customer {
 	
 	private String firstName;
@@ -21,6 +23,17 @@ public class Customer {
 	
 	@Pattern(regexp="^[a-zA-Z0-9]{0,5}", message="only 5 chars/digits")
 	private String postalCode;
+	
+	@CourseCode(value="lin2", message="must start with lin2")
+	private String courseCode;
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
 
 	public Integer getFreePasses() {
 		return freePasses;
